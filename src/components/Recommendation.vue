0f44e308ca2a0e1306d60a11a186dbad1fc2a759
Lucian Petriuc <lpetriuc@iu.edu>
2022-10-18 23:39:29 -0400

    Rough Draft second try

---
+++ src/components/Recommendation.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  ItemImage: {
+    type: Image,
+    required: true,
+  },
+  ItemName: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template lang="">
+<div class="recommendations-card">
+    <img src={{ItemImage}} alt="item image" id="itemId" />
+    <p> {{ItemName}}</p>
+    <nav></nav>
+</div>
+</template>
+
+<style scoped>
+
+.recommendations-card
+{
+  width: 200px;
+  position:relative;
+  height: 200px;
+  background: var(--color-highlight);
+  margin: 10px 5px;
+  padding: 20px 15px;
+  display: flex;
+  box-shadow: 0.5px 10px;
+  transition: 0.3s ease-in-out;
+  margin-top: 5%;
+  flex-direction: column;
+  right: 130%;
+}
+
+</style>
+
