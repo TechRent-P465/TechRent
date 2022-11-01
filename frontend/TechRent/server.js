2b9238a61c6dcfa726db24236a6c35cf20ba1d4e
Krisha <krisha.bm@somaiya.edu>
2022-11-01 17:08:17 -0400

    Heroku config

--- frontend/TechRent/server.js
+++ frontend/TechRent/server.js
@@ -5,4 +5,4 @@ const app=express()
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
 const port=process.env.PORT || 8080
 app.listen(port)
-console.log('Listening on port:'+port)
+console.log(`app is listening on port: ${port}`)
