d9976201c4ec94f757a1c4be36de92fa08703b7b
cam-line <camline333@gmail.com>
2022-12-06 16:41:05 -0500

    Final commit on this branch

--- frontend/TechRent/src/App.vue
+++ frontend/TechRent/src/App.vue
@@ -8,7 +8,25 @@
       <router-link to="/browse">Browse</router-link>
       <router-link v-if="isAuthenticated" to="/post">Post Item</router-link>
       <router-link v-if="isAdmin" to="/admin/items">Admin</router-link>
+      <button v-if="userData.name" class="username" @click="openNav">
+        Welcome, {{ userData.name }}
+      </button>
+      <div id="mySidenav" class="sidenav">
+        <ul>
+          <li><a @click="closeNav" class="closebtn">&times;</a></li>
+          <li>
+            <a>Rented Items</a>
+          </li>
+          <li>
+            <a>Account Details</a>
+          </li>
+          <li>
+            <a>Messages</a>
+          </li>
+        </ul>
+      </div>
     </nav>
+    <div class="all-over-bkg"></div>
   </header>
   <div class="content-wrapper">
     <router-view id="content" />
@@ -33,6 +51,7 @@
 //import { RouterLink, RouterView } from "vue-router";
 import LogoLink from '@/components/LogoLink.vue'
 import BrowseItems from '@/Views/BrowseItems.vue'
+import { mapState } from 'vuex'
 
 export default {
   mounted() {
@@ -64,6 +83,15 @@ export default {
       } else {
         e.preventDefault()
       }
+    },
+    ...mapState(['userData']),
+    openNav() {
+      document.querySelector('#mySidenav').style.width = '250px'
+      document.querySelector('.all-over-bkg').classList.add('is-visible')
+    },
+    closeNav() {
+      document.querySelector('#mySidenav').style.width = '0'
+      document.querySelector('.all-over-bkg').classList.remove('is-visible')
     }
   }
 }
@@ -127,6 +155,81 @@ nav {
   font-size: 1rem;
   padding: 1rem 0;
   margin-top: 1rem;
+}
+
+/*
+  Navigation
+*/
+.openbtn {
+  color: var(--aqua-black);
+}
+
+.openbtn:hover {
+  cursor: pointer;
+  color: var(--aqua-blue);
+}
+
+.sidenav ul {
+  margin: 0;
+  padding: 0;
+}
+
+.sidenav li {
+  list-style-type: none;
+}
+
+.all-over-bkg {
+  display: none;
+}
+
+.is-visible {
+  background-color: rgba(0, 0, 0, 0.4);
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  z-index: 9999;
+  display: block;
+}
+
+#mySidenav {
+  z-index: 10000;
+}
+
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right: 0;
+  overflow-x: hidden;
+  padding-top: 60px;
+  transition: 0.5s;
+  background-color: var(--color-background);
+}
+
+.sidenav ul li a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  display: block;
+  transition: 0.3s;
+  color: var(--color-secondary);
+}
+
+.sidenav .closebtn {
+  /* position: absolute;
+  top: 0;
+  right: 25px; */
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+.subnav li a {
+  font-size: 16px;
+  margin-left: 1em;
 }
 
 @media (min-width: 1024px) {
